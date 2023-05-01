16c2c0b8caaeff82d2e22b3a9d31d76405835a61
George <georgemishchenko@gmail.com>
2023-05-01 15:15:01 +0200

    Menu component created

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,13 @@
+import logo from './logo.jpg'
+import { Link } from 'react-router-dom'
+
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <header>
+      <img src={logo} />
+      <Link to="/">Home</Link>
+      <a target="_blank" href="https://github.com/xaxaxage/weather-web-site">Github</a>
+    </header>
   )
 }
 
