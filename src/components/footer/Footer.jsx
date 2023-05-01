a0e2b6c301900ec21cc950e4ba6de911eae01eab
George <georgemishchenko@gmail.com>
2023-05-01 16:05:01 +0200

    Footer component created

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <a target="_blank" href="https://www.weatherapi.com/">Weather API service</a>
+      <a target="_blank" href="https://https://github.com/xaxaxage">Github account</a>
+    </footer>
+  )
+}
+
+export default Footer
